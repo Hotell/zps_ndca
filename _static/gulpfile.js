e017df025bc6bdfd373e0697ffb7bb33433898fc
Martin Hotell <hochelmartin@gmail.com>
2014-01-21 21:14:12 +0100

    chore(scaffold) - prepare build tasks etc

--- _static/gulpfile.js
+++ _static/gulpfile.js
@@ -2,4 +2,49 @@
  * Created by Hotell on 20.1.2014.
  */
 "use strict";
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var usemin = require('gulp-usemin');
+var less = require('gulp-less');
 
+var fs = require('fs');
+var path = require('path');
+
+
+gulp.task('scripts', function () {
+    // Minify and copy all JavaScript (except vendor scripts)
+    return gulp.src(['js/**/*.js', '!js/vendor/**'])
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
+});
+
+// Copy all static images
+gulp.task('images', function () {
+    return gulp.src('images/**')
+        // Pass in options to the task
+        .pipe(imagemin({optimizationLevel: 5}))
+        .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('less', function () {
+    gulp.src('./css/less/**/*.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest('./css'));
+});
+
+// The default task (called when you run `gulp`)
+gulp.task('default', function () {
+    gulp.run('scripts', 'images');
+
+    // Watch files and run tasks if they change
+    gulp.watch('client/js/**', function () {
+        gulp.run('scripts');
+    });
+
+    gulp.watch('client/img/**', function () {
+        gulp.run('images');
+    });
+});
