d0d3d4b33c45e739d95eeb1b76e24ed3dda4bdd8
Martin Hotell <hochelmartin@gmail.com>
2014-02-20 23:51:26 +0100

    chore(grunt) - add connect + live reload

--- _static/Gruntfile.js
+++ _static/Gruntfile.js
@@ -3,23 +3,61 @@ module.exports = function (grunt) {
     // load all grunt tasks matching the `grunt-*` pattern
     require('load-grunt-tasks')(grunt);
 
-    var watchCfg = {
-        less: {
-            files: ['css/less/*.less'],
-            tasks: ['less:dev']
-        }
-    };
 
     var cfg = {
         pkg: grunt.file.readJSON('package.json'),
-        watch: watchCfg,
+        src: {
+            less: 'css/less'
+        },
+        watch: {
+            styles: {
+                files: ['<%= src.less %>/*.less'],
+                tasks: ['newer:less:dev', 'autoprefixer']
+            },
+            gruntfile: {
+                files: ['Gruntfile.js']
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    './*.html',
+                    '.tmp/styles/{,*/}*.css',
+                    './images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+                    './js/*.js'
+                ]
+            }
+        },
+        // The actual grunt server settings
+        connect: {
+            options: {
+                port: 9000,
+                // Change this to '0.0.0.0' to access the server from outside.
+                hostname: 'localhost',
+                livereload: 35729
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    base: [
+                        '.tmp',
+                        './'
+                    ]
+                }
+            },
+            dist: {
+                options: {
+                }
+            }
+        },
         less: {
             dev: {
                 options: {
                     paths: ["css/less"]
                 },
                 files: {
-                    "css/style.css": "css/less/style.less"
+                    "css/style.css": "<%= src.less %>/style.less"
                 }
             },
             dist: {
@@ -31,10 +69,33 @@ module.exports = function (grunt) {
                     "path/to/result.css": "path/to/source.less"
                 }
             }
+        },
+        // Add vendor prefixed styles
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions']
+            },
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: '.tmp/styles/',
+                        src: '{,*/}*.css',
+                        dest: '.tmp/styles/'
+                    }
+                ]
+            }
         }
     };
 
     grunt.initConfig(cfg);
+
+    grunt.registerTask('serve', [
+        'less:dev',
+        'autoprefixer',
+        'connect:livereload',
+        'watch'
+    ]);
     grunt.registerTask('default', []);
 
 };
